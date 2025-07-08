fbf50816d08838015c822eef7e37dc44bea61ceb
Abhinav6677 <abhikashyap7560100@gmail.com>
2025-07-08 06:28:32 +0530

    increased accuracy

--- data_model.py
+++ data_model.py
@@ -3,15 +3,13 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class Model(nn.Module):
-    def __init__(self, input_size=4, h1=8, h2=10, output_size=3):
-        super(Model, self).__init__()
-        self.fc1 = nn.Linear(input_size, h1)
-        self.fc2 = nn.Linear(h1, h2)
-        self.out = nn.Linear(h2, output_size)
+    def __init__(self):
+        super().__init__()
+        self.fc1 = nn.Linear(4, 16)
+        self.out = nn.Linear(16, 3)
         
     def forward(self, x):
         x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
         x = self.out(x)
         return x
 
